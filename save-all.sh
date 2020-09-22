31b88f82b38e0b41362b7118226e073fdaca55e3
Kimly Koh <kimly@kimlykoh.com>
2020-09-21 22:51:27 -0700

    changed

--- save-all.sh
+++ save-all.sh
@@ -2,8 +2,8 @@
 
 cd `dirname $0`
 
-find . -type d | while read l ; do git add $l ; done
-find . -type f | while read l ; do git add $l ; done
+find . -type d | grep -v .git | while read l ; do git add $l ; done
+find . -type f | grep -v .git | while read l ; do git add $l ; done
 
 git commit -am "changed $(now ) "
 git push
