0877b606d5b95ec870bd98dfef145f581760134c
Kimly Koh <kimly@kimlykoh.com>
2020-11-20 14:42:35 -0800

    new website

---
+++ script.js
@@ -0,0 +1,13 @@
+var firstName=""
+var email=""
+var comment=""
+
+
+function saveMessage(){
+   firstName=document.getElementById("firstName").value;
+   email=document.getElementById("email").value;
+   comment=document.getElementById("comment").value;
+   console.log(firstName)
+   console.log(email)
+   console.log(comment)
+    }
