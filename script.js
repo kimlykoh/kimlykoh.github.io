99ee72ff7f68e60a0dfc89ee23d59aa16638815a
Kimly Koh <kimly@kimlykoh.com>
2020-11-27 15:30:28 -0800

    updated website

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-var firstName=""
-var email=""
-var comment=""
+var firstName="";
+var email="";
+var comment="";
+var option=""; 
 
 
 function saveMessage(){
    firstName=document.getElementById("firstName").value;
    email=document.getElementById("email").value;
    comment=document.getElementById("comment").value;
-   console.log(firstName)
-   console.log(email)
-   console.log(comment)
+   console.log(firstName);
+   console.log(email);
+   console.log(comment);
     }
+
+
+function curryPuffOptions(){
+   option=document.getElementById("curry-puff-recipe").value;
+   document.getElementById("curry-puff-recipe-button").textContent="Read " + option + " Recipe";
+   console.log("Read" + option + "Recipe");
+  
+}
+
+document.getElementById("temp").textContent="Kimly's Creation";
+
+function greetings(){
+   document.getElementById("temp").textContent="Authentic Malaysian Appetizer";
+}
+
