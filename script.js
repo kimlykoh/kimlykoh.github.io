58f28f47344418daeade0d5a7864bec5d6dc456a
Kimly Koh <kimly@kimlykoh.com>
2020-12-18 15:26:27 -0800

    update

--- script.js
+++ script.js
@@ -17,12 +17,19 @@ function saveMessage(){
 function curryPuffOptions(){
    option=document.getElementById("curry-puff-recipe").value;
    document.getElementById("curry-puff-recipe-button").textContent="Read " + option + " Recipe";
-   console.log("Read" + option + "Recipe");
-  
+   document.getElementById("curry-puff-recipe-button").setAttribute("href", option + "-curry-puff.html" )
+}
+
+
+function satayOptions(){
+   option=document.getElementById("satay-recipe").value;
+   document.getElementById("satay-recipe-button").textContent="Read " + option + " Recipe";
+   document.getElementById("satay-recipe-button").setAttribute("href", option + "-satay.html" )
+   document.getElementById("satay-desc").textContent=option + " Skewer";
 }
 
 document.getElementById("temp").textContent="Kimly's Creation";
-
+document.getElementById("firstName").setAttribute("placeholder","First Name")
 function greetings(){
    document.getElementById("temp").textContent="Authentic Malaysian Appetizer";
 }
